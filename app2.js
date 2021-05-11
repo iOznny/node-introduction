99216fb63246e4f9b5332bf6aa577debc6044cd7
iOznny <f000@gm.com>
2021-05-11 16:04:33 -0500

    cambio

---
+++ app2.js
@@ -0,0 +1,5 @@
+const greet = (name) => {
+    return `Saludos ${ name }`;
+}
+
+console.log(greet('Demo'));
---
+++ app3.js
@@ -0,0 +1,17 @@
+console.log('Inicio');
+
+//Callback: Es una función que se manda como
+// argumento dentro de otra función.
+setTimeout(() => {
+    console.log('Primer timeout');
+}, 3000);
+
+setTimeout(() => {
+    console.log('Segundo timeout');
+}, 0);
+
+setTimeout(() => {
+    console.log('Tercero timeout');
+}, 0);
+
+console.log('Fin');
