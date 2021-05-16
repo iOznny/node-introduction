945306f003dbad2d0007370ea7f8ab1479cd9753
iOznny <f000170014@gmail.com>
2021-05-15 19:32:07 -0500

    c

--- promesas.js
+++ promesas.js
@@ -1,15 +1,15 @@
 const employees = [
     {
         id: 1,
-        name: 'Demo'
+        name: 'Demo1'
     },
     {
         id: 2,
-        name: 'Demo'
+        name: 'Demo2'
     },
     {
         id: 3,
-        name: 'Demo'
+        name: 'Demo3'
     }
 ];
 
@@ -24,18 +24,50 @@ const salarys = [
     }
 ];
 
-const getEmploye = (id, callback) => {
-    const promesa = new Promise((resolve, reject ) => {
-        const employee = employees.find(e => e .id === id)?.name;
+const id = 10;
 
-        if (employee) {
-            
-        }
+const getEmploye = (id) => {
 
-
+    return new Promise((resolve, reject ) => {
+        const employee = employees.find(e => e.id === id)?.name;
+        (employee) ? resolve(employee) : reject(`No existe un empleado con el id: `, id);
     });
-  
 }
 
-getEmploye(1)
-    .then(employe => console.log(employe));
+const getSalary = (id) => {
+    return new Promise((resolve, reject) => {
+        const salary = salarys.find(s => s.id === id)?.salary;
+        (salary) ? resolve(salary) : reject(`No existe un salario con el id: `, id);
+    });
+}
+
+
+
+/* getEmploye(id)
+    .then(employe => console.log(employe))
+    .catch(err => console.log(err));
+
+getSalary(id)
+    .then(salary => console.log(salary))
+    .catch(err => console.log(err)); */
+
+/* getEmploye(id)
+    .then(employe => {
+        getSalary(id).then(salary => {
+            console.log('El empleado:', employe, 'tiene un salario de:', salary);
+        })
+    })
+    .catch(err => console.log(err)); */
+
+
+
+let name;
+
+// Promesas en cadena
+getEmploye(id)
+    .then(employee => {
+        name = employee;
+        return getSalary(id);
+    })
+    .then(salary => console.log('El empleado:', name, 'tiene un salario de:', salary))
+    .catch(err => console.log(err));
